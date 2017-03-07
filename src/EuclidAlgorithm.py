ccf8089c48c0787dd5f6bdb958aee5bdaf5246de
songziheng <songziheng@xiaomi.com>
2017-03-07 11:34:22 +0800

    v1.0

---
+++ src/EuclidAlgorithm.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+class EuclidAlgorithm:
+    """
+    欧几里得算法 求最大公约数
+    """
+    def __init__(self):
+        pass
---
+++ src/MaxSubArray.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+class MaxSubArray:
+    "最大和子序列的和求解"
+    def __init__(self):
+        pass
+    def algorithm0(self):
+        pass
+    def algorithm1(self):
+        pass
+    def algorithm2(self):
+        pass
+    def algorithm3(self):
+        pass
