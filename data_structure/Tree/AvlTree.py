df4bf0c9ab5765fa9ff487bb21519142d7c993a4
nana_g55 <ziheng_song@126.com>
2017-03-26 15:04:18 +0800

    ss

--- data_structure/Tree/AvlTree.py
+++ data_structure/Tree/AvlTree.py
@@ -6,7 +6,7 @@ import random
 class AvlNode(bstNode):
     def __init__(self, val, height=-1):
         bstNode.__init__(self, val)
-        self.height = -1
+        self.height = height
 
     def left_height(self):
         return self.left.height if self.left is not None else -1
@@ -90,8 +90,8 @@ class AvlTree(BinarySearchTree):
                     assert False, "程序有bug 居然儿子不是自己的？？？？接盘了？？"
                 is_left = True if left_tree_height > right_tree_height else False
                 b = (bottom_node.left if is_left else bottom_node.right)
-                is_single_rotate = True if ((is_left and (b.right_height < b.left_height)) or
-                ((not is_left) and (b.right_height > b.left_height))) else False
+                is_single_rotate = True if ((is_left and (b.right_height() < b.left_height())) or
+                ((not is_left) and (b.right_height() > b.left_height()))) else False
                 # 左子树的外侧最高 或 右子树的外侧最高 时选择单旋转 否则选择双旋转
 
                 # 根据上一步骤的分析进行相应的旋转
@@ -173,23 +173,31 @@ class Avl_Test:
     2 判断这个树是否仍然是一个完整的二叉排序树(中序遍历是否是一个有序的序列)
     """
     def __init__(self):
-        self.node_height_dict = {}
         self.traverse_list = []
 
     def mark_nodes_height(self, root):
+        node_height_dict = {}
+        self.mark_height(root, node_height_dict)
+        return node_height_dict
+
+    def mark_height(self, root, node_height_dict):
         """
         返回一个(node,height) 的元组列表 这个列表里面包含了所有的节点的引用
         相当于记录了
         :param root:
+        :param node_height_dict:
         :return:
         """
         if root is None:
-            self.node_height_dict[id(None)] = (None, -1)  # 添加None节点 高度为-1
+            node_height_dict[id(None)] = (None, -1)  # 添加None节点 高度为-1
             return -1
 
-        left_height = self.mark_nodes_height(root.left)
-        right_height = self.mark_nodes_height(root.right)
-        self.node_height_dict[id(root)] = (root, max(left_height, right_height)+1)
+        left_height = self.mark_height(root.left, node_height_dict)
+        right_height = self.mark_height(root.right, node_height_dict)
+        my_height = max(left_height, right_height) + 1
+        node_height_dict[id(root)] = (root, my_height)
+        return my_height
+
 
     def height_balance_test(self, root):
         """
@@ -197,28 +205,34 @@ class Avl_Test:
         :param root:
         :return:
         """
-        self.mark_nodes_height(root)
-        for node_id in self.node_height_dict:
-            left_id = id(self.node_height_dict[node_id][0].left)
-            right_id = id(self.node_height_dict[node_id][0].right)
-            assert abs(self.node_height_dict[left_id][1] - self.node_height_dict[left_id][1]) < 2, \
-                "height balance not meet"
+        node_height_dict = self.mark_nodes_height(root)
+        for node_id in node_height_dict:
+            if node_id != id(None):
+                left_id = id(node_height_dict[node_id][0].left)
+                right_id = id(node_height_dict[node_id][0].right)
+                assert abs(node_height_dict[left_id][1] - node_height_dict[right_id][1]) < 2, \
+                    "height balance not meet"
+
+    def part_tree2order(self, root, extern_list):
+        if root is not None:
+            self.part_tree2order(root.left, extern_list)
+            extern_list.append(root.val)
+            self.part_tree2order(root.right, extern_list)
 
     def bst2arr(self, root):
-        if root is not None:
-            self.bst2arr(root.left)
-            self.traverse_list.append(root.val)
-            self.bst2arr(root.right)
+        res_list = []
+        self.part_tree2order(root, res_list)
+        return res_list
 
     def order_test(self, root, init_list):
         """
         测试是否仍然是一颗排序树
         :return:
         """
-        self.bst2arr(root)
+        traverse_list = self.bst2arr(root)
         init_copy = init_list.copy()
         init_copy.sort()
-        assert self.traverse_list == init_copy, "order test failure"
+        assert traverse_list == init_copy, "order test failure"
 
     def gen_testcases(self):
         test_cases = []
@@ -243,13 +257,17 @@ class Avl_Test:
         for init_list in testcases:
             tree_tool = AvlTree()
             root = tree_tool.createAvlTree(init_list)
+            # debug
+            print(init_list)
+            tree_tool.print_tree(root)
+
             self.order_test(root, init_list)
             self.height_balance_test(root)
         print("all test passed successfully!")
-
 
 # Test
 if __name__ == "__main__":
     t = Avl_Test()
     t.main_testbench()
+
 
