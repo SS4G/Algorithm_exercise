9686d7fbdad631f1edabcf352aad2ddf804b78f3
songziheng <songziheng@xiaomi.com>
2017-04-01 11:17:19 +0800

    v2.0 of line cnt

--- common_script/line_cnt.py
+++ common_script/line_cnt.py
@@ -15,16 +15,18 @@ def get_all_file(base_dir, file_list=None):
             print("这是什么文件？？")
     return None
 
+def get_file_type(file_name):
+    return file_name.split(".")[-1]
 
 if __name__ == "__main__":
     # arguments
     # 要计算的基础目录
-    base_dir = "/home/mi/workspace0"  # os.getcwd()
+    base_dir = "/home/mi/SS4G/Alf_linux2/AlgorithmTraining"  # os.getcwd()
     # 要统计的文件扩展名（仅限于文本文件 二进制文件不行）
-    cnt_file_type = ["scala", "java", "py", "xml", "js", "sh"]
-    extname_set = set([])
+    cnt_file_type = ["java", "py", "c"]
+    # extname_set = set([])
 
-    print(base_dir)
+    print("statistic directory is :",base_dir)
     filename_list = []
     all_files = get_all_file0(base_dir)
     # extname_set = set(filter(lambda str:len(str)<10, [name.split(".")[-1] for name in all_files]))
@@ -32,8 +34,17 @@ if __name__ == "__main__":
 
     useful_files = filter(lambda s: s.split(".")[-1] in cnt_file_type, all_files)
     cnt = 0
+
+    CountByType_dict = {}
+    for type in cnt_file_type:
+        CountByType_dict[type] = 0
+
     for file in useful_files:
         f = open(file, "r", encoding="utf-8")
-        cnt += len(f.readlines())
+        CountByType_dict[get_file_type(file)] += len(f.readlines())
         f.close()
-    print("total lines is :", cnt)
+    total = 0
+    for type in CountByType_dict:
+        print("type ", type, "  has :", CountByType_dict[type], " lines")
+        total += CountByType_dict[type]
+    print("total lines is :", total)
