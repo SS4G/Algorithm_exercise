006da4903fa3df9a691a4d649dea5f51dba29ac0
SS4G <ziheng_song@126.com>
2017-05-09 22:34:33 +0800

    tmp saving

---
+++ leetcode/java_src/Leet572.java
@@ -0,0 +1,36 @@
+package AlgorithmTraining.leetcode.java_src;
+
+/**
+ * Created by BUPT_SS4G on 2017/5/9.
+ */
+
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+public class Leet572 {
+    public static void getSpicifyNode(TreeNode s, )
+    public static void isSubtree(TreeNode s, TreeNode t) {
+        int root_t_val = t.val;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
