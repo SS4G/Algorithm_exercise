f722ec8212cfff6ff52d96c27a79c39cd97b3294
dt G55 <ziheng_song@126.com>
2017-05-12 22:04:00 +0800

    tmp saving 004

--- leetcode/java_src/Leet051.java
+++ leetcode/java_src/Leet051.java
@@ -5,6 +5,10 @@ package AlgorithmTraining.leetcode.java_src;
  */
 import java.util.*;
 public class Leet051 {
+    private static final int ROW = 0;
+	private static final int COLUMN = 1;
+	private static final int POS_LINE = 2;
+	private static final int NEG_LINE = 3;
     public static List<List<String>> solveNQueens(int n) {
         List<int[]> resArr = new ArrayList<int[]>(1024);
         final int dim = n;
@@ -12,29 +16,24 @@ public class Leet051 {
         int this_r = 0;
         int this_c = 0;
         int alreadyPlacedAmount = 0;
-        TreeSet<Integer> useless_r = new TreeSet<Integer>();//不可放置的行
-        TreeSet<Integer> useless_c = new TreeSet<Integer>();//不可放置的列
-        TreeSet<Integer> useless_l = new TreeSet<Integer>();//不可放置的斜线
+
+     	TreeSet<Integer> uselessROW = new TreeSet<Integer>();
+     	TreeSet<Integer> uselessCOL = new TreeSet<Integer>();
+     	TreeSet<Integer> uselessPOS = new TreeSet<Integer>();
+     	TreeSet<Integer> uselessNEG = new TreeSet<Integer>();
 
         Extern_Loop:
         while (true) {
-            // assert this_c < dim && this_r < dim
-            System.out.println("c = "+this_c+" r = "+this_r);
-            System.out.println("setSize = "+useless_c.size());
             tmpMat[this_c] = this_r;
-            //System.out.println("c = "+this_c+" r = "+this_r);
-            //以下只修改下次要写入的内容 而不修改tmpMat结果
-            if (conflict(this_r, this_c, useless_r, useless_c, useless_l)) {
+            if (conflict(this_r, this_c, uselessROW, uselessCOL, uselessPOS, uselessNEG)) {
                 this_r++;
-                //System.out.println("non");
-
                 if (this_r == dim) {
                     while (this_r >= dim) {
                         this_c--;
                         if (this_c < 0)
                             break Extern_Loop;
                         else
-                            deleteFromRecord(tmpMat[this_c], this_c, useless_r, useless_c, useless_l);
+                            deleteFromRecord(tmpMat[this_c], this_c, uselessROW, uselessCOL, uselessPOS, uselessNEG);
                         this_r = tmpMat[this_c] + 1;
                     }
                 }
@@ -44,7 +43,6 @@ public class Leet051 {
                     int[] tmp = new int[dim];
                     copyArray(tmp, tmpMat);
                     resArr.add(tmp);//add result
-                    //this_c = dim - 1;
                     this_r++;
                     if (this_r == dim) {
                         while (this_r >= dim) {
@@ -52,47 +50,43 @@ public class Leet051 {
                             if (this_c < 0)
                                 break Extern_Loop;
                             else
-                                deleteFromRecord(tmpMat[this_c], this_c, useless_r, useless_c, useless_l);
+                                deleteFromRecord(tmpMat[this_c], this_c, uselessROW, uselessCOL, uselessPOS, uselessNEG);
                             this_r = tmpMat[this_c] + 1;
                         }
                     }
                 }
                 else {
-                    addToRecord(this_r, this_c, useless_r, useless_c, useless_l);
+                    addToRecord(this_r, this_c, uselessROW, uselessCOL, uselessPOS, uselessNEG);
                     this_c++;
                     this_r = 0;
                 }
             }
         }
-        /*for (int[] a : resArr) {
-            for (int j : a) {
-                System.out.print(j+",");
-            }
-            System.out.println("");
-        }*/
         return plotResult(resArr);
     }
 
-    private static void addToRecord(int r, int c, TreeSet<Integer> useless_r,
-                        TreeSet<Integer> useless_c, TreeSet<Integer> useless_l) {
-        useless_r.add(r);
-        useless_c.add(c);
-        useless_l.add(r-c); //save r-c
-        useless_l.add(r+c);
+    private static void addToRecord(int r, int c, TreeSet<Integer> uselessROW, TreeSet<Integer> uselessCOL,
+                                    TreeSet<Integer> uselessPOS, TreeSet<Integer> uselessNEG) {
+        uselessROW.add(r);
+        uselessCOL.add(c);
+        uselessPOS.add(r+c);
+        uselessNEG.add(r-c);
     }
 
-    private static void deleteFromRecord(int r, int c, TreeSet<Integer> useless_r,
-                        TreeSet<Integer> useless_c, TreeSet<Integer> useless_l) {
-        useless_r.remove(r);
-        useless_c.remove(c);
-        useless_l.remove(r-c); //save r-c
-        useless_l.remove(r+c);
+    private static void deleteFromRecord(int r, int c, TreeSet<Integer> uselessROW, TreeSet<Integer> uselessCOL,
+                                    TreeSet<Integer> uselessPOS, TreeSet<Integer> uselessNEG) {
+        uselessROW.remove(r);
+        uselessCOL.remove(c);
+        uselessPOS.remove(r+c);
+        uselessNEG.remove(r-c);
     }
 
-    private static boolean conflict(int this_r, int this_c, TreeSet<Integer> useless_r,
-                                    TreeSet<Integer> useless_c,TreeSet<Integer> useless_l) {
-        return useless_r.contains(this_r) || useless_c.contains(this_c)
-                || useless_l.contains(this_r - this_c) || useless_l.contains(this_c + this_r);
+    private static boolean conflict(int r, int c,  TreeSet<Integer> uselessROW, TreeSet<Integer> uselessCOL,
+                                    TreeSet<Integer> uselessPOS, TreeSet<Integer> uselessNEG) {
+        return  uselessROW.contains(r) ||
+                uselessCOL.contains(c) ||
+                uselessPOS.contains(r+c) ||
+                uselessNEG.contains(r-c);
     }
 
     private static void copyArray(int[] dst, final int[] src) {
@@ -102,7 +96,6 @@ public class Leet051 {
     }
 
     public static List<List<String>> plotResult(List<int[]> resArr) {
-        // resArr[r] = c point at r row c column
         int dim=0;
 
         List<List<String>> result = new LinkedList<List<String>>();
@@ -119,11 +112,9 @@ public class Leet051 {
 
             for (int j = 0; j < dim; j++) //rotate the matrix
                 mat[mat0[j]] = j;
-            //mat = mat0;
 
             oneMat = new ArrayList<String>(dim);
             for (int r = 0; r < dim; r++) {
-                //System.out.println(mat[r]);
                 for (int c = 0; c < dim; c++) {
                     if (c == mat[r])
                         oneRow[c] = 'Q';
