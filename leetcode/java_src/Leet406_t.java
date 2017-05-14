c203584545284ba9798fbd82b3fd33a0bbe3ebc5
dt G55 <ziheng_song@126.com>
2017-05-14 22:17:59 +0800

    tmp saving 006

--- leetcode/java_src/Leet406_t.java
+++ leetcode/java_src/Leet406_t.java
@@ -20,28 +20,71 @@ class Leet406 {
         for (int i = 0; i < people.length; i++) {
             queue[i] = new People(people[i][0], people[i][1], 0);
         }
+        int queueNow = 1;
+        int insertIndex = 0;
+        int lastModify = 0;
+        for (int j = 0; j < queue.length; j++)
+            queue[j].satisfied = getGreater(queue, j, queue[j].height);
 
 
+
+        while (queueNow < people.length) {
+            insertIndex = queueNow - 1;
+            People insertPeople = queue[queueNow];
+            lastModify = queueNow + 1;
+
+            while (true) {
+                System.out.println(insertIndex);
+                if (!(insertPeople.satisfied > insertPeople.demand
+                    || (insertPeople.height >= queue[insertIndex].height
+                            && queue[insertIndex].satisfied < queue[insertIndex].demand)))
+                        break;
+
+                queue[insertIndex+1] = queue[insertIndex];
+                if (queue[insertIndex+1].height < insertPeople.height) {
+                    queue[insertIndex+1].satisfied++;
+                    lastModify = insertIndex+1;
+                }
+                else {
+                    insertPeople.satisfied--;
+                }
+                insertIndex--;
+            }
+            queue[insertIndex+1] = insertPeople;
+            queueNow = lastModify;//ready to move next element
+        }
+        int[][] res = new int[people.length][2];
+        for (int i = 0; i < people.length; i++) {
+                res[i][0] = queue[i].height;
+                res[i][1] = queue[i].demand;
+        }
+        return res;
     }
-    private void swapTwoPeople(People[] queue, int peopleAIndex, int peopleBIndex) {
-        // peopleA , peopleB -> peopleB , peopleA
-        People tmpPeople = queue[peopleAIndex];
-        queue[peopleAIndex] = queue[peopleBIndex];
-        queue[peopleBIndex] = tmpPeople;
-        if (queue[peopleAIndex].height >= queue[peopleBIndex].height) {
-            queue[peopleBIndex].demand += 1;
+    private void showQueue(People[] queue) {
+        for (People a : queue) {
+            System.out.print("height = "+a.height+ " ");
         }
     }
-    private int fixQueue(int[][] queue, int startIndex, int endIndex, int justInserted) {
-        //satisfied: the number satisfied for the current people now
-        //startIndex: the index to start check (include)
-        //endIndex: the index to end check (exclude)
-        //justInserted: the people height just inserted
-
+    private int getGreater(People[] queue, int endIndex, int thisHeight) {
+        //endindex exclude
+        int cnt = 0;
+        for (int index = 0; index < endIndex; index++) {
+            if (queue[index].height >= thisHeight)
+                cnt ++;
+        }
+        return cnt;
     }
 }
 public class Leet406_t {
     public static void main(String[] args) {
         Leet406 s = new Leet406();
+        int[][] people = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+        int[][] res = s.reconstructQueue(people);
+        for (int[] a : res) {
+            for (int b: a) {
+                System.out.print(b+",");
+            }
+            System.out.println("");
+        }
     }
 }
