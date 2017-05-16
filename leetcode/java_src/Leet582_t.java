c1d5060ca276705d3f8d8bfaa067ecae54c27691
SS4G <ziheng_song@126.com>
2017-05-16 12:35:46 +0800

    tmp saving 007

---
+++ leetcode/java_src/Leet582_t.java
@@ -0,0 +1,17 @@
+package AlgorithmTraining.leetcode.java_src;
+
+/**
+ * Created by BUPT_SS4G on 2017/5/16.
+ */
+import java.util.*;
+class Leet582 {
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        return null;
+        //$todo unsolved problem
+    }
+}
+public class Leet582_t {
+    public static void main(String[] args) {
+
+    }
+}
