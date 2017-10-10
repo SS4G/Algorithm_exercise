41e01345f9a9274707997211835188c8800d07cb
SS4G <ziheng_song@126.com>
2017-10-10 18:23:10 +0800

    ss

--- exercise/leetcodeXiaoXiang2/Leet076x_t.java
+++ exercise/leetcodeXiaoXiang2/Leet076x_t.java
@@ -3,9 +3,36 @@ package AlgorithmTraining.exercise.leetcodeXiaoXiang2;
 /**
  * Created by BUPT_SS4G on 2017/10/10.
  */
-class Leet076 {
+import java.util.*;
+class Leet076x {
     public String minWindow(String s, String t) {
+        Map<Character, Integer> charCnt = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            if (!charCnt.containsKey(c))
+                charCnt.put(c, 0);
+            charCnt.put(c, charCnt.get(c) + 1);
+        }
+        String minWindow = "";
+        int minWindowLen = Integer.MAX_VALUE;
+        int bg = 0;
+        int ed = 0;
+        //ed <= bg
+        while (ed <= bg || bg < s.length()) {
+            if (satisfy(charCnt)) {
+                charCnt.put(s.charAt(ed), charCnt.get(s.charAt(bg)) + 1);
+            }
+            else {
+                charCnt.put(s.charAt(bg), charCnt.get(s.charAt(bg)) + 1);
+            }
+        }
+    }
 
+    private boolean satisfy(Map<Character, Integer> charCnt) {
+        for (Character c : charCnt.keySet()) {
+            if (charCnt.get(c) > 0)
+                return false;
+        }
+        return true;
     }
 }
 
